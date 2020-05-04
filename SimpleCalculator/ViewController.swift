9c38c6840bc8c0a8e00054d0edab762e2eeb29b4
Tri Ardini <tr.ardn@gmail.com>
2020-05-04 10:39:24 +0700

    Initial Commit

---
+++ SimpleCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleCalculator
+//
+//  Created by Tri Ardini on 04/05/20.
+//  Copyright © 2020 BuruhKoding. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
