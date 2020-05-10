a00eb1852f060769cae1c1869a0d93391308b6a6
Tri Ardini <tr.ardn@gmail.com>
2020-05-10 22:43:58 +0700

    initial commit

--- SimpleCalculator/ViewController.swift
+++ SimpleCalculator/ViewController.swift
@@ -10,11 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var height: UITextField!
+    @IBOutlet weak var width: UITextField!
+    @IBOutlet weak var result: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func calculateArea(_ sender: UIButton) {
+        if let h = Double(height.text ?? "0"), let w = Double(width.text ?? "0") {
+            result.text = String(h * w)
+        } else {
+            result.text = "Result tidak valid."
+        }
+    }
+    
+    @IBAction func resetView(_ sender: UIButton) {
+        result.text = ""
+        height.text = ""
+        width.text = ""
+    }
 }
 
